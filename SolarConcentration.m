f6ab024ecb1655da51220c9cad67a4cf299c7a30
IRC-APV <I.R.Cole@lboro.ac.uk>
2018-09-11 06:53:00 +0100

    Correction to bracketing

--- SolarConcentration.m
+++ SolarConcentration.m
@@ -2,32 +2,33 @@
 [pathstr,name,ext] = fileparts(which(mfilename));
 cd(pathstr);
 
+% Focal Length
+f = 10;
+
 %% 2D Parabola:
 % y^2 = 4*f*x
 
-f = 1;
 x = -20:0.1:20;
 y = -20:0.1:20;
 [x,y] = meshgrid(x,y);
-z = (x.^2)./4*f;
+z = (x.^2)./(4*f);
 z(z>20) = NaN;
 figure;
 surf(x,y,z);
 axis equal;
 
-surf2stl('2D Parabola.stl', x,y,z);
+% surf2stl('2D Parabola.stl', x,y,z);
 
 %% 3D Parabola:
 % y^2 + z^2 = 4*f*x
 
-f = 1;
 x = -20:0.1:20;
 y = -20:0.1:20;
 [x,y] = meshgrid(x,y);
-z = (x.^2)./4*f + (y.^2)./4*f;
+z = (x.^2)./(4*f) + (y.^2)./(4*f);
 z(z>20) = NaN;
 figure;
 surf(x,y,z);
 axis equal;
 
-surf2stl('3D Parabola.stl', x,y,z);
+% surf2stl('3D Parabola.stl', x,y,z);
